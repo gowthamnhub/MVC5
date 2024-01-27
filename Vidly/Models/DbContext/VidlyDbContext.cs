57c045122752a4b55d8ac38b7950b507dc87d67e
gowthamnhub <gowthamnhub@gmail.com>
2024-01-27 11:53:34 +0530

    EF added, initial migration added, updated notes for EF

---
+++ Vidly/Models/DbContext/VidlyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models.DbContext
+{
+	public class VidlyDbContext : System.Data.Entity.DbContext
+	{
+        public VidlyDbContext()
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+	}
+}
