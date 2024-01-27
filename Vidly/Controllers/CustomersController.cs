3ff02b4e65444907ba79f299868577198b9f480a
gowthamnhub <gowthamnhub@gmail.com>
2024-01-27 17:16:10 +0530

    deleted files that are needed to be ignored, changes with respect to EF seeding, EagerLoading, Changing the model and notes

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.Models.DbContext;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        List<Customer> customers = new List<Customer>
+        //List<Customer> lstCustomers = new List<Customer>
+        //{
+        //    new Customer { Id = 1001, Name = "Gowthaman" },
+        //    new Customer { Id = 1002, Name = "Kavin" },
+        //    new Customer { Id = 1003, Name = "Sangeetha" }
+        //};
+
+        private VidlyDbContext _context;
+        public CustomersController()
         {
-            new Customer { Id = 1001, Name = "Gowthaman" },
-            new Customer { Id = 1002, Name = "Kavin" },
-            new Customer { Id = 1003, Name = "Sangeetha" }
-        };
+            _context = new VidlyDbContext();//we are not using dependency injection here.
+        }
         // GET: Customers
         public ActionResult Index()
         {
+            var customers = _context.Customers.Include(x=> x.MembershipType) .ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = customers.FirstOrDefault(c => c.Id == id);
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
             return View(customer);
         }
     }
