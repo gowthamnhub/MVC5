f6ed9da39ba371290ae37cfca6868b7cce78b704
gowthamnhub <gowthamnhub@gmail.com>
2024-02-07 22:01:49 +0530

    refactoring magic numbers using two static fields in Membership type for 0 and 1 (unknown, payAsYouGo)

--- Vidly/Models/Minimum18YearsAge.cs
+++ Vidly/Models/Minimum18YearsAge.cs
@@ -6,32 +6,33 @@ using System.Web;
 
 namespace Vidly.Models
 {
-    public class Minimum18YearsAge : ValidationAttribute
+  public class Minimum18YearsAge : ValidationAttribute
+  {
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            //check if the membership selected is pay-as-you-go, if not then check age limit, if yes, just return success
-            //so to check the other property here(MemberShipType), this valiationContext object helps.
-            var customer = (Customer)validationContext.ObjectInstance;
-            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
-                return ValidationResult.Success;
+      //check if the membership selected is pay-as-you-go, if not then check age limit, if yes, just return success
+      //so to check the other property here(MemberShipType), this valiationContext object helps.
+      var customer = (Customer)validationContext.ObjectInstance;
+      if (customer.MembershipTypeId == MembershipType.Unknown ||
+          customer.MembershipTypeId == MembershipType.PayAsYouGo) return ValidationResult.Success;
 
-            if (customer.BirthDate == null)
-                return new ValidationResult("Birth date is required");
-            var age = GetAgeFromBirthDate(customer.BirthDate);
+      if (customer.BirthDate == null)
+        return new ValidationResult("Birth date is required");
+      var age = GetAgeFromBirthDate(customer.BirthDate);
 
-            if(age >= 18) return ValidationResult.Success;
+      if (age >= 18) return ValidationResult.Success;
 
-            return new ValidationResult("Age should be at least 18 years to be on a member");
-        }
+      return new ValidationResult("Age should be at least 18 years to be on a member");
 
-        private static int GetAgeFromBirthDate(DateTime ?customerBirthDate)
-        {
-           var age = DateTime.Today.Year - customerBirthDate.Value.Year;
-           if (DateTime.Today.Month < customerBirthDate.Value.Month) return age - 1;
-           if (DateTime.Today.Day < customerBirthDate.Value.Day) return age-1;
-           return age;
-
-        }
     }
+
+    private static int GetAgeFromBirthDate(DateTime? customerBirthDate)
+    {
+      var age = DateTime.Today.Year - customerBirthDate.Value.Year;
+      if (DateTime.Today.Month < customerBirthDate.Value.Month) return age - 1;
+      if (DateTime.Today.Day < customerBirthDate.Value.Day) return age - 1;
+      return age;
+
+    }
+  }
 }
