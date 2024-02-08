89a068206fa40680fb0a080ab52221210eeeac8f
gowthamnhub <gowthamnhub@gmail.com>
2024-02-08 08:01:47 +0530

    Again made the create/update calls same save method, cleaned up some unwanted files (Edit.cshtml, removed update action method) to keep code clean and simple

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,96 +10,76 @@ using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class CustomersController : Controller
+  public class CustomersController : Controller
+  {
+    private VidlyDbContext _context;
+    public CustomersController()
     {
-        //List<Customer> lstCustomers = new List<Customer>
-        //{
-        //    new Customer { Id = 1001, Name = "Gowthaman" },
-        //    new Customer { Id = 1002, Name = "Kavin" },
-        //    new Customer { Id = 1003, Name = "Sangeetha" }
-        //};
-
-        private VidlyDbContext _context;
-        public CustomersController()
-        {
-            _context = new VidlyDbContext();//we are not using dependency injection here.
-        }
-        // GET: Customers
-        public ActionResult Index()
-        {
-            var customers = _context.Customers.Include(x => x.MembershipType).ToList();
-            return View(customers);
-        }
-
-        public ActionResult Details(int id)
-        {
-            var customer = _context.Customers.Include(x => x.MembershipType).FirstOrDefault(c => c.Id == id);
-            return View(customer);
-        }
-
-        public ActionResult Create()
-        {
-            var membershipTypes = _context.MembershipTypes.ToList();
-            var newCustomerViewModel = new CustomerFormViewModel()
-            {
-                Customer = new Customer(),
-                MembershipTypes = membershipTypes
-            };
-            return View(newCustomerViewModel);
-        }
-        [HttpPost]
-        public ActionResult Create(Customer customer)
-        {
-            if (!ModelState.IsValid)
-            {
-                var viewModel = new CustomerFormViewModel
-                {
-                    Customer = customer,
-                    MembershipTypes = _context.MembershipTypes.ToList()
-                };
-
-                return View(viewModel);
-            }
-            _context.Customers.Add(customer);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]//Post does not seem to be supported from view, as formMethod I can set only to FormMethod.POST or FormMethod.GET
-        public ActionResult Update(Customer customer)
-        {
-            if (!ModelState.IsValid)
-            {
-                var viewModel = new CustomerFormViewModel
-                {
-                    Customer = customer,
-                    MembershipTypes = _context.MembershipTypes.ToList()
-                };
-
-                return View("Edit", viewModel);
-            }
-            else
-            {
-                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
-                //TryUpdateModel or autoMapper - but these updates entire model, sometimes we may need only specific properties to be updated
-                customerInDb.Name = customer.Name;
-                customerInDb.MembershipTypeId = customer.MembershipTypeId;
-                customerInDb.BirthDate = customer.BirthDate;
-                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-            }
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Edit(int id)
-        {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
-            CustomerFormViewModel viewModel = new CustomerFormViewModel
-            {
-                Customer = customer,
-                MembershipTypes = _context.MembershipTypes.ToList()
-            };
-            return View(viewModel);
-        }
+      _context = new VidlyDbContext();//we are not using dependency injection here.
     }
+    // GET: Customers
+    public ActionResult Index()
+    {
+      var customers = _context.Customers.Include(x => x.MembershipType).ToList();
+      return View(customers);
+    }
+
+    public ActionResult Details(int id)
+    {
+      var customer = _context.Customers.Include(x => x.MembershipType).FirstOrDefault(c => c.Id == id);
+      return View(customer);
+    }
+
+    public ActionResult Create()
+    {
+      var membershipTypes = _context.MembershipTypes.ToList();
+      var newCustomerViewModel = new CustomerFormViewModel()
+      {
+        Customer = new Customer(),
+        MembershipTypes = membershipTypes
+      };
+      return View("CustomerForm", newCustomerViewModel);
+    }
+
+    [HttpPost]//Put does not seem to be supported from view, as formMethod I can set only to FormMethod.POST or FormMethod.GET
+    public ActionResult Save(Customer customer)
+    {
+      if (!ModelState.IsValid)
+      {
+        var viewModel = new CustomerFormViewModel
+        {
+          Customer = customer,
+          MembershipTypes = _context.MembershipTypes.ToList()
+        };
+
+        return View("CustomerForm", viewModel);
+      }
+      if (customer.Id == 0)
+      {
+        _context.Customers.Add(customer);
+      }
+      else
+      {
+        var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+        //TryUpdateModel or autoMapper - but these updates entire model, sometimes we may need only specific properties to be updated
+        customerInDb.Name = customer.Name;
+        customerInDb.MembershipTypeId = customer.MembershipTypeId;
+        customerInDb.BirthDate = customer.BirthDate;
+        customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+      }
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+      CustomerFormViewModel viewModel = new CustomerFormViewModel
+      {
+        Customer = customer,
+        MembershipTypes = _context.MembershipTypes.ToList()
+      };
+      return View("CustomerForm", viewModel);
+    }
+  }
 }
