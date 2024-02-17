551a72aea9155c7c768b066e3ae931b597f049bd
gowthamnhub <gowthamnhub@gmail.com>
2024-02-17 21:40:59 +0530

    added CustomerDto manually and map it before add new customer and save

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Vidly.Models;
 using Vidly.Models.DbContext;
+using Vidly.Models.Dtos;
 
 namespace Vidly.Controllers.Api
 {
@@ -36,42 +35,44 @@ namespace Vidly.Controllers.Api
 
     [HttpPost]
     //api/customers
-    public Customer Add(Customer customer)
+    public Customer Add(CustomerDto customer)
     {
       if (!ModelState.IsValid)
       {
         throw new HttpResponseException(HttpStatusCode.BadRequest);
       }
-      else
+
+      //Map customer Dto to customer properties
+      //we have to add customer to context.customers
+      var newCustomer = new Customer
       {
-        //we have to add customer to context.customers
-        var newCustomer = _context.Customers.Add(customer);
-        _context.SaveChanges();
-        return newCustomer;
-      }
+        Name = customer.Name,
+        BirthDate = customer.BirthDate,
+        IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter,
+        MembershipType = customer.MembershipType,
+        MembershipTypeId = customer.MembershipTypeId
+      }; 
+      _context.Customers.Add(newCustomer);
+      _context.SaveChanges();
+      return newCustomer;
     }
 
     [HttpPut]
     //api/customers/1 (with request body) 
-    public Customer Update(int id, Customer customer)
+    public Customer Update(int id, CustomerDto customer)
     {
       var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
-      if (existingCustomer == null)
-      {
-        throw new HttpResponseException(HttpStatusCode.NotFound);
-      }
-      else
-      {
-        //update each properties for the customer
-        //instead of assigning these values individually, we can use tools to auto map these properties like AutoMapper.
-        existingCustomer.Name = customer.Name;
-        existingCustomer.BirthDate = customer.BirthDate;
-        existingCustomer.MembershipTypeId = customer.MembershipTypeId;
-        existingCustomer.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+      if (existingCustomer == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+      existingCustomer.Name = customer.Name;
+      existingCustomer.BirthDate = customer.BirthDate;
+      existingCustomer.MembershipTypeId = customer.MembershipTypeId;
+      existingCustomer.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
 
-        _context.SaveChanges();
-        return existingCustomer;
-      }
+      _context.SaveChanges();
+      return existingCustomer;
+
+      //update each properties for the customer
+      //instead of assigning these values individually, we can use tools to auto map these properties like AutoMapper.
     }
 
     [HttpDelete]
@@ -83,15 +84,11 @@ namespace Vidly.Controllers.Api
       {
         throw new HttpResponseException(HttpStatusCode.NotFound);
       }
-      else
-      {
-        //remove customer from the DB
-        _context.Customers.Remove(existingCustomer); //this object will be marked as removed in memory
-        _context.SaveChanges();
-        return existingCustomer;
-      }
+
+      //remove customer from the DB
+      _context.Customers.Remove(existingCustomer); //this object will be marked as removed in memory
+      _context.SaveChanges();
+      return existingCustomer;
     }
-
-
   }
 }
---
+++ Vidly/Models/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models.Dtos
+{
+  public class CustomerDto
+  { public int Id { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+
+    [Display(Name = "Subscribed to Newsletter")]
+    public bool IsSubscribedToNewsletter { get; set; }
+
+    public MembershipType MembershipType { get; set; }
+
+    [Display(Name = "Membership Type")]
+    public byte MembershipTypeId { get; set; }
+
+    //[Minimum18YearsAge]
+    [Display(Name = "Date of Birth")]
+    public DateTime? BirthDate { get; set; }
+  }
+}
