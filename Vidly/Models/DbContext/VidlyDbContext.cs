e49e80514e14712384ba800c6aff58a0c0e447ff
gowthamnhub <gowthamnhub@gmail.com>
2024-03-07 07:26:59 +0530

    Basic changes to add Identity, installed required packages for Identity, OWIN

--- Vidly/Models/DbContext/VidlyDbContext.cs
+++ Vidly/Models/DbContext/VidlyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,16 +7,21 @@ using System.Web;
 
 namespace Vidly.Models.DbContext
 {
-	public class VidlyDbContext : System.Data.Entity.DbContext
-	{
-        public VidlyDbContext()
-        {
-            
-        }
+  public class VidlyDbContext : IdentityDbContext<ApplicationUser>
+    {
+    public VidlyDbContext()
+    {
 
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-	}
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<MembershipType> MembershipTypes { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+
+    public static VidlyDbContext Create()
+    {
+      return new VidlyDbContext();
+    }
+  }
 }
